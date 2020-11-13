0cbafb3908092b53821d2fd59c551c15d2d178b8
June-Junior <artknownuntist@gmail.com>
2020-11-13 06:09:09 +0200

    commit 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+import './index.html';
+import menuSource from './menu.json';
+import menuTemplate from './templates/menu-items.hbs';
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+const menuList = document.querySelector('.js-menu');
+
+const markup = menuTemplate(menuSource);
+menuList.insertAdjacentHTML('beforeend', markup);
